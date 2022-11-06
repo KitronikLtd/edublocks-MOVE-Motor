9fb27a46a1232e7ec9186b9f8ac325a0e603cfc8
Joshua Lowe <joshualowe1002@gmail.com>
2022-11-06 23:28:41 +0000

    Update generators.ts

--- generators.ts
+++ generators.ts
@@ -9,17 +9,19 @@ Blockly.Python['move_motor_init'] = function(block) {
   return code;
 };
 
+Blockly.Python['move_motor_speed'] = function(block) {
+  var text_text = block.getFieldValue('slider');
+  var code = text_text;
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
 Blockly.Python['move_motor_direction'] = function(block) {
   var variable_buggy = Blockly.Python.variableDB_.getName(block.getFieldValue('buggy'), Blockly.Variables.NAME_TYPE);
   var dropdown_motor_side = block.getFieldValue('motor_side');
-  var dropdown_motor_direction = block.getFieldValue('motor_direction');
   var value_motor_speed = Blockly.Python.valueToCode(block, 'motor_speed', Blockly.Python.ORDER_ATOMIC);
-  if (dropdown_motor_direction == "backward")
-    value_motor_speed *= -1;
-  var code = variable_buggy+ '.' + dropdown_motor_side + '(' + value_motor_speed + ')\n';
+  var code = variable_buggy + '.' + dropdown_motor_side + '(' + value_motor_speed + ')\n';
   return code;
 };
-
 
 Blockly.Python['move_motor_stop'] = function(block) {
   var variable_buggy = Blockly.Python.variableDB_.getName(block.getFieldValue('buggy'), Blockly.Variables.NAME_TYPE);
